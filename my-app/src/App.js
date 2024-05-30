0257f51d3c1679a887cff7c4724e498e5ce99b99
andeugeni <andeugeni@gmail.com>
2024-05-30 00:04:38 -0700

    Added Footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
+// import './App.css';
+
+// import Navbar from './components/Navbar';
 import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Footer from './components/Footer'
+// import Services from './components/pages/Services';
+// import Products from './components/pages/Products';
+// import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>WEBSITE IN DEVELOPMENT</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://www.instagram.com/aarinxandycreate/?hl=en"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          MEOW
-        </a>
-      </header>
-    </div>
+    <>
+     
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <code>WEBSITE IN DEVELOPMENT</code>
+          </p>
+          <a
+            className="App-link"
+            href="https://www.instagram.com/aarinxandycreate/?hl=en"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            MEOW
+          </a>
+        </header>
+      </div>
+      <Footer />
+    </>
+    
+    
+    
   );
 }
 
---
+++ my-app/src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import Footer from '../Footer';
+
+function Home() {
+    return (
+        <>
+            <Footer />
+            
+        </>
+    );
+}
+
+export default Home
