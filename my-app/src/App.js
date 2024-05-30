490f823b045d113a635c26f47d1abdd78ff1da99
andeugeni <andeugeni@gmail.com>
2024-05-29 20:13:11 -0700

    Added Js Files for each sub-page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>MEOW MEOW MEOW</code> and save to reload.
+          Edit <code>MEOWZA</code> and save to reload.
         </p>
         <a
           className="App-link"
