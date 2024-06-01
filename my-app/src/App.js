55841827da4084d38f998619f721faea658e6e8a
andeugeni <andeugeni@gmail.com>
2024-06-01 15:53:01 -0700

    nav bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,34 +3,46 @@ import logo from './logo.svg';
 
 // import Navbar from './components/Navbar';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 import Home from './components/pages/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Footer from './components/Footer'
-// import Services from './components/pages/Services';
-// import Products from './components/pages/Products';
-// import SignUp from './components/pages/SignUp';
+import About from './components/pages/About';
+import Arrangements from './components/pages/Arrangements';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
     <>
-     
-      <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <p className='App-title-text'>
-            AARIN x ANDY CREATE
-          </p>
-          <a
-            className="App-link"
-            href="https://www.instagram.com/aarinxandycreate/?hl=en"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            WEBSITE IN DEVELOPMENT
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+          <Route path='/About' element={<About/>} />
+          <Route path='/Arrangements' element={<Arrangements/>} />
+          <Route path='/Contact' element={<Contact/>} />
+        </Routes>
+{/*       
+        <div className="App">
+          <header className="App-header">
+            <p className='App-title-text'>
+              AARIN x ANDY CREATE
+            </p>
+            <a
+              className="App-link"
+              href="https://www.instagram.com/aarinxandycreate/?hl=en"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              WEBSITE IN DEVELOPMENT
+            </a>
+          </header>
+        </div>
+         */}
+      </Router>
       <Footer />
+      
     </>
     
     
---
+++ my-app/src/components/pages/About.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Footer from '../Footer';
+import '../../App.css';
+
+function About() {
+    return (
+        <>
+            <div className="App">
+                <header className="App-header">
+                <p className='App-title-text'>
+                    AARIN x ANDY CREATE
+                </p>
+                <a
+                    className="App-link"
+                    href="https://www.instagram.com/aarinxandycreate/?hl=en"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    WEBSITE IN DEVELOPMENT
+                </a>
+                </header>
+            </div>  
+            
+        </>
+    );
+}
+
+export default About
