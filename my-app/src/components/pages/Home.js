59b28f84c6c3207ce2b733c11c081cc63abebd3d
andeugeni <andeugeni@gmail.com>
2024-06-05 14:13:36 -0700

    font style on homepage

--- my-app/src/components/pages/Home.js
+++ my-app/src/components/pages/Home.js
@@ -7,20 +7,21 @@ function Home() {
         <>
             <div className="App">
                 <header className="App-header">
-                <p className='App-title-text'>
-                    AARIN x ANDY CREATE
-                </p>
-                <a
-                    className="App-link"
-                    href="https://www.instagram.com/aarinxandycreate/?hl=en"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    WEBSITE IN DEVELOPMENT
-                </a>
+                    <p className='App-title-text'>
+                        AARIN x ANDY CREATE
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://www.instagram.com/aarinxandycreate/?hl=en"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        text-decoration="none"
+                    >
+                        CHECK US OUT
+                    </a>
                 </header>
-            </div>  
-            
+            </div>
+
         </>
     );
 }
