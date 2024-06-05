8d85ccdc87436253b9af9337a401d64221a03eb5
andeugeni <andeugeni@gmail.com>
2024-06-05 14:13:24 -0700

    two column for contact + latest project

--- my-app/src/components/pages/Contact.js
+++ my-app/src/components/pages/Contact.js
@@ -3,6 +3,8 @@ import '../../App.css';
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import '../Contact.css'
+import '../TwoColumn.css'
+import EmbeddedVideo from '../EmbeddedVideo'
 
 
 function Contact() {
@@ -42,17 +44,33 @@ function Contact() {
                 <input type="submit" value="Send" />
             </form> */}
 
-            <div className='paragraph-pink'>
+            <div className='Paragraph-pink'>
+                <div className='row'>
+                    <div className='column'>
+                        <div className='box'>
+                            <form className="form" ref={form} onSubmit={sendEmail}>
+                                <div className='form-h2'>CONTACT US</div>
+                                <p type="Name:"><input name="user_name" placeholder="Write your name here.."></input></p>
+                                <p type="Email:"><input name="user_email" placeholder="Let us know how to contact you back.."></input></p>
+                                <p type="Message:"><input name="message" placeholder="What would you like to tell us.."></input></p>
+                                <button type="submit">Send Message</button>
+                            </form>
+                        </div>
+
+                    </div>
+                    <div className='column'>
+                        <div className='box'>
+                            Check out our latest project here:
+                            <EmbeddedVideo embedId="GmCLttye9gA" />
+                        </div>
+                        
+                    </div>
+                </div>
+
                 {/* <form className="form" ref={form} onSubmit={sendEmail}> */}
-                <form className="form" ref={form} onSubmit={sendEmail}>
-                    <div className='form-h2'>CONTACT US</div>
-                    <p type="Name:"><input name="user_name" placeholder="Write your name here.."></input></p>
-                    <p type="Email:"><input name="user_email" placeholder="Let us know how to contact you back.."></input></p>
-                    <p type="Message:"><input name="message" placeholder="What would you like to tell us.."></input></p>
-                    <button type="submit">Send Message</button>
-                </form>
+
             </div>
-            
+
 
 
 
