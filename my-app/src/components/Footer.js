57314ca68539bf607afd42f85c4527857ca149ad
andeugeni <andeugeni@gmail.com>
2024-06-13 21:56:36 -0700

    new footer?

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,56 +1,55 @@
 import React from 'react'
 import './Footer.css';
-import { TiSocialInstagram } from "react-icons/ti";
-import { TiSocialYoutube } from "react-icons/ti";
-import { Button } from './Button';
+import {
+    TiSocialInstagram,
+    TiSocialYoutube,
+    TiSocialInstagramCircular,
+    TiSocialYoutubeCircular,
+ } from "react-icons/ti";
+ import { AiFillSpotify } from "react-icons/ai";
+import { HiMusicalNote, } from "react-icons/hi2";
 
+
+import { Button } from './Button';
 import { Link } from 'react-router-dom';
+import './TwoColumn.css';
+// import '../App.css'
+
 
 function Footer() {
     return (
-        
-        <div className='footer-container'>
-            <section className='footer-subscription'>
-                <p className='footer-subscription-heading'>
-                    Check out our creations!!
-                </p>
-                {/* <p className='footer-subscription-text'>
-                    But, this email prompt does nothing... unfortunately...
-                </p>
-                <div className='input-areas'>
-                    <form>
-                        <input
-                            className='footer-input'
-                            name='email'
-                            type='email'
-                            placeholder='Your Email'
-                        />
-                        <Button buttonStyle='btn--outline'>Subscribe</Button>
-                    </form>
-                </div>
-                <a
-                    className="App-link"
-                    href="https://www.instagram.com/aarinxandycreate/?hl=en"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    text-decoration="none"
-                >
-                    CHECK US OUT
-                </a> */}
 
-            </section>
-            <div class="footer-icons">
-                
-                    <a href="https://www.instagram.com/aarinxandycreate/?hl=en" class="fa fa-instagram"><TiSocialInstagram /></a>
-                    <a href="https://youtube.com/playlist?list=PLBxAcBEuRqUngW0fcESVjdS3OCTZhVO_Z&si=8ACJx3Nq4xhGA-YD" class="fa fa-youtube"><TiSocialYoutube /></a>
-                
+        <div className="footer-container">
+            <div className='row'>
+                <div className='column'>
+                    <div className='box'>
+                        <div className='footer-subscription-text'>
+                        <div className='footer-logo'>
+                            Aarin x Andy
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='column'>
+                    <div className='box'>
+                        <div className="footer-icons">
+                            {/* <ul>
+                                <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/aarinxandycreate/?hl=en" class="fa fa-ig"><TiSocialInstagram /></a>
+                                <a target="_blank" rel="noopener noreferrer" href="https://youtube.com/playlist?list=PLBxAcBEuRqUngW0fcESVjdS3OCTZhVO_Z&si=8ACJx3Nq4xhGA-YD" class="fa fa-yt"><TiSocialYoutube /></a>
+                                <a target="_blank" rel="noopener noreferrer" href="https://open.spotify.com/playlist/4CLdUK0eff4vEaoptx9GvF?si=0530166dd74d4fba" class="fa fa-spotify"><AiFillSpotify /></a>
+                                <a target="_blank" rel="noopener noreferrer" href="https://youtube.com/playlist?list=PLBxAcBEuRqUngW0fcESVjdS3OCTZhVO_Z&si=8ACJx3Nq4xhGA-YD" class="fa fa-music"><HiMusicalNote /></a>
+                            </ul> */}
+                            <ul>
+                                <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/aarinxandycreate/?hl=en" class="fa fa-ig"><TiSocialInstagram /></a>
+                                <a target="_blank" rel="noopener noreferrer" href="https://youtube.com/playlist?list=PLBxAcBEuRqUngW0fcESVjdS3OCTZhVO_Z&si=8ACJx3Nq4xhGA-YD" class="fa fa-yt"><TiSocialYoutube /></a>
+                                <a target="_blank" rel="noopener noreferrer" href="https://open.spotify.com/playlist/4CLdUK0eff4vEaoptx9GvF?si=0530166dd74d4fba" class="fa fa-spotify"><AiFillSpotify /></a>                            </ul>
+                                {/* <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/aarinxandycreate/?hl=en" class="fa fa-music" ><HiMusicalNote /></a> */}
+                        </div>
+                    </div>
+                </div>
             </div>
-            {/* <section className='footer-subscription'>
-                <p className='footer-subscription-text'>
-                    Photo: Andre Liu (2019)
-                </p>
-            </section> */}
         </div>
+
     );
 }
 
