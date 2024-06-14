244f270b989f109af220024923165e3b172d62ff
andeugeni <andeugeni@gmail.com>
2024-06-14 14:50:47 -0700

    put items far apart and centered social icons on mobile view

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -5,14 +5,13 @@ import {
     TiSocialYoutube,
     TiSocialInstagramCircular,
     TiSocialYoutubeCircular,
- } from "react-icons/ti";
- import { AiFillSpotify } from "react-icons/ai";
+} from "react-icons/ti";
+import { AiFillSpotify } from "react-icons/ai";
 import { HiMusicalNote, } from "react-icons/hi2";
 
 
 import { Button } from './Button';
 import { Link } from 'react-router-dom';
-import './TwoColumn.css';
 // import '../App.css'
 
 
@@ -20,17 +19,17 @@ function Footer() {
     return (
 
         <div className="footer-container">
-            <div className='row'>
-                <div className='column'>
+            <div className='footer-row'>
+                <div className='left-column'>
                     <div className='box'>
                         <div className='footer-subscription-text'>
-                        <div className='footer-logo'>
-                            Aarin x Andy
-                        </div>
+                            <div className='footer-logo'>
+                                Aarin x Andy
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className='column'>
+                <div className='right-column'>
                     <div className='box'>
                         <div className="footer-icons">
                             {/* <ul>
@@ -42,8 +41,10 @@ function Footer() {
                             <ul>
                                 <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/aarinxandycreate/?hl=en" class="fa fa-ig"><TiSocialInstagram /></a>
                                 <a target="_blank" rel="noopener noreferrer" href="https://youtube.com/playlist?list=PLBxAcBEuRqUngW0fcESVjdS3OCTZhVO_Z&si=8ACJx3Nq4xhGA-YD" class="fa fa-yt"><TiSocialYoutube /></a>
-                                <a target="_blank" rel="noopener noreferrer" href="https://open.spotify.com/playlist/4CLdUK0eff4vEaoptx9GvF?si=0530166dd74d4fba" class="fa fa-spotify"><AiFillSpotify /></a>                            </ul>
-                                {/* <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/aarinxandycreate/?hl=en" class="fa fa-music" ><HiMusicalNote /></a> */}
+                                <a target="_blank" rel="noopener noreferrer" href="https://open.spotify.com/playlist/4CLdUK0eff4vEaoptx9GvF?si=0530166dd74d4fba" class="fa fa-spotify"><AiFillSpotify /></a>
+                            </ul>
+
+                            {/* <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/aarinxandycreate/?hl=en" class="fa fa-music" ><HiMusicalNote /></a> */}
                         </div>
                     </div>
                 </div>
