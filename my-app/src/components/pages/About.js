1b3f509358b82967d4d49c2293f567d22b4b5c7c
andeugeni <andeugeni@gmail.com>
2024-12-16 18:18:44 -0800

    about page updated

--- my-app/src/components/pages/About.js
+++ my-app/src/components/pages/About.js
@@ -7,33 +7,22 @@ function About() {
         <>
             <div className="Paragraph-pink">
                 <p className='Paragraph-header'>
-                    LOREM IPSUM
+                    WHO ARE WE?
                 </p>
                 <p className='Paragraph-text'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                    irure dolor in reprehenderit in voluptate velit esse cillum
-                    dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                    non proident, sunt in culpa qui officia deserunt mollit anim id 
-                    est laborum.
+                We are AarinxAndy, two arrangers who first met in college whose musical styles and convictions melded like hand in glove… if you didn’t go to USC, that is. Aarin brings technical acumen and a decade of jazz performance experience while Andrew brought a unique perspective based on structural patterns in EDM, and the audacity to apply it elsewhere. Their first arrangement - “E.T” by Katy Perry - won Outstanding Arrangement at ICCA for Pitch, Please! (from UCLA) and the two realized their potential together.
+Our specialty is crafting unique arrangements that breathe fresh life into every piece we touch, blending unexpected elements and fusing different genres to create something unique. With a deep appreciation for artistry and a flair for the unconventional, we aim to transform familiar tunes into unforgettable experiences.
+
                 </p>
             </div>
 
             <div className="Paragraph-purple">
                 <p className='Paragraph-header'>
-                    LOREM IPSUM
+                    EPIC MEOW
                 </p>
                 <p className='Paragraph-text'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                    irure dolor in reprehenderit in voluptate velit esse cillum
-                    dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                    non proident, sunt in culpa qui officia deserunt mollit anim id 
-                    est laborum.
+                We’ve been absolutely grateful to work for some amazing groups, one of which is DaCadence from the University of Maryland. Our work on their closer in 2023 and the whole set in 2024 was brought to the ICCA Finals stage in New York. Anyways I hope you enjoy our creations and if you want to work with us, hit that Contact button :)
+
                 </p>
             </div>
             
